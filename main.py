df2713becb5104827b199dd70a79955def4d4a9e
Simão Gato <simao.valedegato@gmail.com>
2025-08-31 15:12:51 +0100

    feat: accept book paths from command line

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from stats import get_book_text, num_of_words, get_count_of_characters, sort_dict
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+
     book_text = get_book_text(book_path)
     word_count = num_of_words(book_text)
-    # print(f"{word_count} words found in the document")
     char_count = get_count_of_characters(book_text)
     sorted_char_count = sort_dict(char_count)
-    # print(char_count)
+    
+    # pretty report
     print("============ BOOKBOT ============\n" \
     f"Analyzing book found at {book_path}...\n"
     "----------- Word Count ----------\n"
     f"Found {word_count} total words\n"
     "--------- Character Count -------")
+
     for item in sorted_char_count:
         if item["char"].isalpha():
             print(f"{item['char']}: {item['num']}")
+
     print("============= END ===============")
 
 main()
